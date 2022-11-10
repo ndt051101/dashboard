5e246c676338c2d3aaa81cb4f13d55e85e50195b
tiendn2k1 <duytienabc123@gmail.com>
2022-11-10 22:54:42 +0700

    refactor code

--- src/components/layout/Sidenav.js
+++ src/components/layout/Sidenav.js
@@ -1,4 +1,4 @@
-import {Menu, Button} from "antd";
+import {Menu} from "antd";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faChartLine, faUser, faBook, faSignsPost} from '@fortawesome/free-solid-svg-icons';
 import {NavLink, useLocation} from "react-router-dom";
@@ -7,32 +7,6 @@ import logo from "../../assets/images/logo.png";
 function Sidenav({color}) {
   const {pathname} = useLocation();
   const page = pathname.replace("/", "");
-
-  const dashboard = [
-    <svg
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"
-      key={0}
-    >
-      <path
-        d="M3 4C3 3.44772 3.44772 3 4 3H16C16.5523 3 17 3.44772 17 4V6C17 6.55228 16.5523 7 16 7H4C3.44772 7 3 6.55228 3 6V4Z"
-        fill={color}
-      ></path>
-      <path
-        d="M3 10C3 9.44771 3.44772 9 4 9H10C10.5523 9 11 9.44771 11 10V16C11 16.5523 10.5523 17 10 17H4C3.44772 17 3 16.5523 3 16V10Z"
-        fill={color}
-      ></path>
-      <path
-        d="M14 9C13.4477 9 13 9.44771 13 10V16C13 16.5523 13.4477 17 14 17H16C16.5523 17 17 16.5523 17 16V10C17 9.44771 16.5523 9 16 9H14Z"
-        fill={color}
-      ></path>
-    </svg>,
-  ];
-
-
-
 
   const profile = [
     <svg
@@ -151,7 +125,7 @@ function Sidenav({color}) {
           </NavLink>
         </Menu.Item>
         <Menu.Item className="menu-item-header" key="5">
-          Account Pages
+          Trang tài khoản
         </Menu.Item>
         <Menu.Item key="6">
           <NavLink to="/profile">
@@ -163,39 +137,22 @@ function Sidenav({color}) {
             >
               {profile}
             </span>
-            <span className="label">Profile</span>
+            <span className="label">Thông tin</span>
           </NavLink>
         </Menu.Item>
         <Menu.Item key="7">
           <NavLink to="/sign-in">
             <span className="icon">{signin}</span>
-            <span className="label">Sign In</span>
+            <span className="label">Đăng nhập</span>
           </NavLink>
         </Menu.Item>
         <Menu.Item key="8">
           <NavLink to="/sign-up">
             <span className="icon">{signup}</span>
-            <span className="label">Sign Up</span>
+            <span className="label">Đăng xuất</span>
           </NavLink>
         </Menu.Item>
       </Menu>
-      <div className="aside-footer">
-        <div
-          className="footer-box"
-          style={{
-            background: color,
-          }}
-        >
-          <span className="icon" style={{color}}>
-            {dashboard}
-          </span>
-          <h6>Need Help?</h6>
-          <p>Please check our docs</p>
-          <Button type="primary" className="ant-btn-sm ant-btn-block">
-            DOCUMENTATION
-          </Button>
-        </div>
-      </div>
     </>
   );
 }
